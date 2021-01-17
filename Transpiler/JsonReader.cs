8260e7beefb6c8f4cfb26a1bd36370241fd26ec7
Влад Концевич <kontsevichv@mail.ru>
2021-01-17 19:57:14 +0300

    Implemented replacement of lines of code with equivalent lines by pattern

---
+++ Transpiler/JsonReader.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Transpiler
+{
+	public class JsonReader
+	{
+		private string _filePath;
+
+		public JsonReader(string filePath)
+		{
+			_filePath = filePath;
+		}
+
+		public string GetValue(string key)
+		{
+			string json = Read();
+			var data = (JObject)JsonConvert.DeserializeObject(json);
+
+			var a = data.Properties().ToList();
+
+			return data[key].Value<string>();
+		}
+
+		public List<string> GetKeys()
+		{
+			string json = Read();
+			var data = (JObject)JsonConvert.DeserializeObject(json);
+
+			return (from property in data.Properties()
+					select property.Name).ToList();
+		}
+
+		private string Read()
+		{
+			return File.ReadAllText(_filePath);
+		}
+	}
+}
---
+++ Transpiler/Test.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+
+main()
+{
+	int fahr;
+
+	for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+	{
+		printf(fahr);
+	}
+
+	while (fahr < 300)
+	{
+		fahr = fahr + 10;
+
+		if (fahr > 0)
+		{
+			printf(fahr);
+		}
+	}
+}
